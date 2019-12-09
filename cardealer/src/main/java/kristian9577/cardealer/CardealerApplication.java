38e0a7b4eefddff8d3aa7ef8c3cb21d60ad07e2c
ghost <blackghost2693@gmail.com>
2019-12-09 16:30:33 +0200

    Add enable scheduling annotation

--- cardealer/src/main/java/kristian9577/cardealer/CardealerApplication.java
+++ cardealer/src/main/java/kristian9577/cardealer/CardealerApplication.java
@@ -2,8 +2,10 @@ package kristian9577.cardealer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CardealerApplication {
 
     public static void main(String[] args) {
